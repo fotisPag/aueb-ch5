8442f4e5209e078f307d43f3284f6b593292dd97
fotisPag <fotis.pagonis01@gmail.com>
2024-03-17 21:46:45 +0200

    RightTriangle part1

---
+++ RightTriangle.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.ch5;
+
+import java.util.Scanner;
+
+/*
+* Ελέγχει αν ένα τρίγωνο είναι ορθογώνιο.
+*
+* @author fotisPag
+* */
+
+public class RightTriangle {
+
+    public static void main(String[] args) {
+
+        //Δηλώσεις και αρχικοποιήσεις
+        final double EPSILON = 0.005;
+        Scanner in = new Scanner(System.in);
+        double a,b,c;
+        boolean isRight = false;
+
+        System.out.println("Δώστε τα μήκη των τριών πλευρών του");
+
+    }
+
+}
