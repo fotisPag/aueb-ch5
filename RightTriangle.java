b98957c4070d4b15d1754a9837ce71112f243770
fotisPag <fotis.pagonis01@gmail.com>
2024-03-18 23:03:50 +0200

    RightTriangle vol2

--- RightTriangle.java
+++ RightTriangle.java
@@ -13,12 +13,22 @@ public class RightTriangle {
     public static void main(String[] args) {
 
         //Δηλώσεις και αρχικοποιήσεις
-        final double EPSILON = 0.005;
+        final double EPSILON = 0.05;
         Scanner in = new Scanner(System.in);
         double a,b,c;
         boolean isRight = false;
 
-        System.out.println("Δώστε τα μήκη των τριών πλευρών του");
+        System.out.println("Δώστε τα μήκη των τριών πλευρών του τριγώνου");
+
+        a = in.nextDouble();
+        b = in.nextDouble();
+        c = in.nextDouble();
+
+        //Έλεγχος αν το τρίγωνο είναι ορθογώνιο
+        System.out.printf("%.4f %.4f%n", a*a, (b*b + c*c));
+        isRight = (Math.abs(a*a - b*b - c*c) <= EPSILON);
+
+        System.out.printf("Το τρίγωνο είναι ορθογώνιο", (isRight) ? "" : "ΔΕΝ");
 
     }
 
