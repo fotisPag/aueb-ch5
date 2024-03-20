829b1010c166a55a59f59c99f747ebe6ab80e6c4
fotisPag <fotis.pagonis01@gmail.com>
2024-03-20 20:35:29 +0200

    added MethodAdd

---
+++ MethodAdd.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.ch5;
+
+import java.util.Scanner;
+
+/*
+* Υπολογίζει το άθροισμα δύο ακεραίων αριθμών
+* με τη χρήση μεθόδου
+*
+* @author fotisPag
+* */
+
+
+public class MethodAdd {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int first, second;
+        int sum2;
+
+        do {
+            System.out.println("Δώσε πρώτο αριθμό");
+            first = in.nextInt();
+
+            System.out.println("Δώσε δεύτερο αριθμό");
+            second = in.nextInt();
+
+            sum2 = add(first, second);
+            System.out.println("Το άθροισμα είναι " + sum2);
+        }while(sum2 !=0);
+        System.out.println("Το άθροισμα είναι 0");
+    }
+
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+}
