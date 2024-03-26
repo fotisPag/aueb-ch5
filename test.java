c8f888f696724403bd03714bcf4cc4d0b6f0dbe9
fotisPag <fotis.pagonis01@gmail.com>
2024-03-26 14:13:50 +0200

    little project part2

--- test.java
+++ test.java
@@ -3,10 +3,10 @@ package gr.aueb.cf.ch5;
 public class test {
 
     public static void main(String[] args) {
-        for (int i = 10; i >= 1; i--) {
-            System.out.println("*");
-            for (int j = 1; j <= i; j++) {
-                System.out.println("*");
+        for (int i = 1; i <= 1; i--) {
+            System.out.println("");
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");
             }
         }
 
